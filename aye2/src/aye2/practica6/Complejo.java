7c52c404da751cc159b0c7dc1c0cc6dd7b280bd1
AluDoc <AluDoc@DESKTOP-8GNEA3F>
2018-05-24 11:23:26 -0300

    d

--- aye2/src/aye2/practica6/Complejo.java
+++ aye2/src/aye2/practica6/Complejo.java
@@ -71,7 +71,7 @@ public class Complejo {
 	
 	
 
-}
+
 /*Implemente la clase Complejo (números complejos). Defina constructores y el
 método de multiplicación de complejos. Además programe tres funciones suma(),
 una que reciba dos números de tipo int, otra que reciba dos números de tipo float,
