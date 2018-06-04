80b509c324bc6c754d1d04c024a59c9cf4e50fd4
gabrieldedominicis <gabrieldd94@gmail.com>
2018-06-04 10:16:03 -0300

    fe

---
+++ aye2/src/aye2/practica8/MiAppException.java
@@ -0,0 +1,7 @@
+package aye2.practica8;
+
+public class MiAppException extends Exception {
+	public MiAppException(String msg, Throwable e) {
+		super(msg,e);
+	}
+}
--- aye2/src/aye2/practica8/PruebaExcepciones.java
+++ aye2/src/aye2/practica8/PruebaExcepciones.java
@@ -1,16 +1,56 @@
 package aye2.practica8;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PruebaExcepciones {
 	private static final int SIZE_ARREGLO = 10;
-
-	public static void main(String[] args) {
-		Integer[] arregloInt = new Integer[SIZE_ARREGLO];			
+	private static Integer[] arregloInt;
+	public static void ingresarDatoArray(int indice) {//metodo q la lanza
+		try{
+			arregloInt[indice] = 15;
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("El indice esta fuera de rango");
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+			for (StackTraceElement elemento : e.getStackTrace()) {
+					System.out.println("Archivo: " + elemento.getFileName());//devuelve nombre del archivo
+					System.out.println("Linea de error: " + elemento.getLineNumber());
+					System.out.println("Clase: " + elemento.getClassName());
+		}
+	}
+}
+	public static void ingrsrarDatoArray2(int indice) throws Exception{
+		try {
+			arregloInt[indice] = 15;
+		}catch(ArrayIndexOutOfBoundsException e) {
+			throw new Exception("Nueva Ecxeption por indice fuera de rango", e);
+		}
+	}
+	public static int leerIndice() {
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Ingrese el indice: ");
-		int indice = teclado.nextInt(); //string d datos q se pasa a entero
-		arregloInt[indice] = 15;
+		int indice = 0;
+		boolean ingresoOk = false;
+		while(!ingresoOk) {
+			
+		}
+		try {
+			System.out.println("Ingrese el indice: ");
+			indice = teclado.nextInt();//string d datos q se pasa a entero
+			ingresoOk = true;
+		}catch (InputMismatchException e) {
+			System.out.println("Error en ingreso numerico. Intente nuevamente.");
+			teclado.next();
+		}
+		teclado.close();
+		
+		return indice;
+	}
+	
+	public static void main(String[] args) {
+		Integer[] arregloInt = new Integer[SIZE_ARREGLO];			 
+		ingresarDatoArray(leerIndice()); //metodo q la invoca
+		//arregloInt[indice] = 15;
 		System.out.println("Termina OK");
 	}
 
